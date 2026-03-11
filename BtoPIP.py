74ed873a743c53e52cd4e9c582066bcfc4ac425d
stillnode <stillnode@gmail.com>
2026-03-11 15:41:37 +0100

    Add files via upload

--- BtoPIP.py
+++ BtoPIP.py
@@ -28,9 +28,8 @@ bl_info = {
 import bpy
 import math
 import os
-
-scene = bpy.context.scene
-object = bpy.ops.object
+import numpy as np
+import subprocess
 
 # Project Name property
 bpy.types.Scene.project_name = bpy.props.StringProperty(
@@ -40,23 +39,23 @@ bpy.types.Scene.project_name = bpy.props.StringProperty(
     )
 
 # get all empties
-empties = [o for o in scene.objects if o.type == 'EMPTY']
+empties = [o for o in bpy.context.scene.objects if o.type == 'EMPTY']
 
 # sort empties
 empties.sort(key=lambda o: int(o.name))
 
 # get cameras if any
-cameras = [o for o in scene.objects if o.type == 'CAMERA']
+cameras = [o for o in bpy.context.scene.objects if o.type == 'CAMERA']
 
 # if not create one
 if not cameras:
-    object.camera_add(
+    bpy.ops.object.camera_add(
         location=(0, 0, 0), 
         rotation=(math.pi, math.pi, 0)
     )
 else:
     camera = cameras[0]
-    scene.camera = camera
+    bpy.context.scene.camera = camera
     camera.location=(0, 0, 0)
     camera.rotation_euler=(math.pi/2, 0, 0)    
     
@@ -67,58 +66,92 @@ class BTOPIP_OT_renderbtn(bpy.types.Operator):
     bl_idname = "btopip.renderbtn"
     bl_label = "Render"
     
-    def execute(self, context):
+    def execute(self, context):    
         
         # deselect all
-        object.select_all(action='DESELECT')
+        bpy.ops.object.select_all(action='DESELECT')
                        
         # move camera to each empties then render the 6 images
         for e in empties:
             
-            folder = f"//{scene.project_name}.pipmak/{e.name}/"
+            folder = f"//{bpy.context.scene.project_name}.pipmak/{e.name}/"
             folder = bpy.path.abspath(folder)
             
-            scene.render.resolution_x = 1024
-            scene.render.resolution_y = 1024
-            scene.render.image_settings.file_format = "JPEG"
+            bpy.context.scene.render.resolution_x = 1024
+            bpy.context.scene.render.resolution_y = 1024
+            bpy.context.scene.render.image_settings.file_format = "JPEG"
             
             camera.location = (e.location.x, e.location.y, e.location.z + 1.80)
             camera.data.type = 'PERSP'
             camera.data.lens = 18
             
-            scene.render.filepath = os.path.join(folder, f"n{e.name}_front.jpg")
+            bpy.context.scene.render.filepath = os.path.join(folder, f"n{e.name}_front.jpg")
             camera.rotation_euler = (math.pi/2, 0, 0)
             bpy.ops.render.render(write_still=True)
             
-            scene.render.filepath = os.path.join(folder, f"n{e.name}_right.jpg")
+            bpy.context.scene.render.filepath = os.path.join(folder, f"n{e.name}_right.jpg")
             camera.rotation_euler = (math.pi/2, 0, -(math.pi/2))
             bpy.ops.render.render(write_still=True)
             
-            scene.render.filepath = os.path.join(folder, f"n{e.name}_back.jpg")
+            bpy.context.scene.render.filepath = os.path.join(folder, f"n{e.name}_back.jpg")
             camera.rotation_euler = (math.pi/2, 0, math.pi)
             bpy.ops.render.render(write_still=True)    
             
-            scene.render.filepath = os.path.join(folder, f"n{e.name}_left.jpg")
+            bpy.context.scene.render.filepath = os.path.join(folder, f"n{e.name}_left.jpg")
             camera.rotation_euler = (math.pi/2, 0, math.pi/2)
             bpy.ops.render.render(write_still=True)  
             
-            scene.render.filepath = os.path.join(folder, f"n{e.name}_bottom.jpg")
+            bpy.context.scene.render.filepath = os.path.join(folder, f"n{e.name}_bottom.jpg")
             camera.rotation_euler = (0, 0, 0)
             bpy.ops.render.render(write_still=True)     
             
-            scene.render.filepath = os.path.join(folder, f"n{e.name}_top.jpg")
+            bpy.context.scene.render.filepath = os.path.join(folder, f"n{e.name}_top.jpg")
             camera.rotation_euler = (math.pi, 0, 0)
             bpy.ops.render.render(write_still=True)    
             
-            # write hotspot.png
-            scene.render.resolution_x = 360
-            scene.render.resolution_y = 180
-            scene.render.image_settings.file_format = "PNG"
+            # write hotspot.png          
+                      
+            width, height = 360, 180
+            output_rgb = os.path.join(folder, "hotspots_temp.png")
+            output_indexed = os.path.join(folder, "hotspots.png")
+
+            img = bpy.data.images.new("Hotspots", width=width, height=height)
+
+            pixels = np.zeros((height, width, 4), dtype=np.float32)
             
-            scene.render.filepath = f"//{scene.project_name}.pipmak/{e.name}/hotspots.png"
-            bpy.ops.render.render(write_still=True)    
+            colors = [
+                (1,0,0,1),   # front = red
+                (0,1,0,1),   # right = green
+                (0,0,1,1),   # back = blue
+                (1,1,0,1),   # left = yellow
+                (0,1,1,1),   # top = cyan
+                (1,0,1,1),   # bottom = magenta
+            ]
+
+            section_width = width//4
+            for i in range(4):
+                pixels[height//4:3*height//4, i*section_width:(i+1)*section_width] = colors[i]
+
+            pixels[:height//4, :] = colors[4]
+
+            pixels[3*height//4:, :] = colors[5]
+
+            pixels_flat = pixels.flatten()
+            img.pixels = pixels_flat.tolist()
+
+            img.filepath_raw = output_rgb
+            img.file_format = 'PNG'
+            img.save()
+
+            if os.system("magick -version") == 0:
+                subprocess.run([
+                    "magick", output_rgb,
+                    "-colors", "256",
+                    "-dither", "None",
+                    "PNG8:" + output_indexed
+                ])             
             
-            # write nodes lua file
+            # write node lua file
             lua_text = f""" 
             --This tells Pipmak what images to use to form the cubic panorama node. The
             --panorama is made up of six JPEG images, front, right, back, left, top, and
@@ -149,7 +182,7 @@ class BTOPIP_OT_renderbtn(bpy.types.Operator):
                 
         # write main.lua
         
-        folder = f"//{scene.project_name}.pipmak/"
+        folder = f"//{bpy.context.scene.project_name}.pipmak/"
         folder = bpy.path.abspath(folder)
         
         lua_text = f"""
@@ -157,7 +190,7 @@ class BTOPIP_OT_renderbtn(bpy.types.Operator):
         --The version of the software.
         version (0.27)
         --The title of the software.
-        title "{scene.project_name}"
+        title "{bpy.context.scene.project_name}"
         --The starting node of the software, in this case the menu.
         startnode (1)
         --A function. This function tells Pipmak, on starting a new game, about 
@@ -187,7 +220,7 @@ class BTOPIP_PT_panel(bpy.types.Panel):
     
     def draw(self, context):
         layout = self.layout
-        layout.prop(scene, "project_name")
+        layout.prop(bpy.context.scene, "project_name")
         layout.operator("btopip.renderbtn")
 
 
